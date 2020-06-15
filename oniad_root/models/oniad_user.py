4631f3bdcc425765a5b176a7a9295e4006f7e87d
Informatica Arelux <informatica@arelux.com>
2020-06-15 11:09:45 +0200

    https://grupoarelux.atlassian.net/browse/ODOOONIAD-231

--- oniad_root/models/oniad_user.py
+++ oniad_root/models/oniad_user.py
@@ -646,39 +646,4 @@ class OniadUser(models.Model):
                                             }
                                             mail_activity_obj = self.env['mail.activity'].sudo(crm_lead_vals['user_id']).create(mail_activity_vals)
                                     #update
-                                    self.welcome_lead_id = crm_lead_obj.id                
-    
-    @api.one
-    def action_send_mail_with_template_id(self, template_id=False):
-        if template_id!=False:                                        
-            mail_template_item = self.env['mail.template'].search([('id', '=', template_id)])[0]                                
-            mail_compose_message_vals = {                    
-                'author_id': 1,
-                'record_name': self.name,                                                                                                                                                                                           
-            }
-            #Fix user_id
-            if self.user_id.id>0:
-                mail_compose_message_vals['author_id'] = self.user_id.partner_id.id
-                
-                mail_compose_message_obj = self.env['mail.compose.message'].with_context().sudo(self.user_id.id).create(mail_compose_message_vals)
-            else:            
-                mail_compose_message_obj = self.env['mail.compose.message'].with_context().sudo().create(mail_compose_message_vals)
-                
-            return_onchange_template_id = mail_compose_message_obj.onchange_template_id(mail_template_item.id, 'comment', 'crm.lead', self.id)                                
-            
-            mail_compose_message_obj.update({
-                'author_id': mail_compose_message_vals['author_id'],
-                'template_id': mail_template_item.id,
-                'composition_mode': 'comment',
-                'model': 'crm.lead',
-                'res_id': self.id,
-                'body': return_onchange_template_id['value']['body'],
-                'subject': return_onchange_template_id['value']['subject'],
-                'email_from': return_onchange_template_id['value']['email_from'],
-                'partner_ids': return_onchange_template_id['value']['partner_ids'],
-                #'attachment_ids': return_onchange_template_id['value']['attachment_ids'],
-                'record_name': mail_compose_message_vals['record_name'],
-                'no_auto_thread': False,                     
-            })                                         
-            mail_compose_message_obj.send_mail_action()                                                                                                                
-            return True             
+                                    self.welcome_lead_id = crm_lead_obj.id
